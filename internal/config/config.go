0aeaf83cb4a13034d0a7e6da42c59ca9a7b76228
justcgh9 <a.kabardiyadi@innopolis.university>
2025-10-26 18:17:11 +0300

    update configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,24 +6,21 @@ import (
 	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Env		string		`yaml:"env" env-default:"local"`
-	Http 	HttpConfig	`yaml:"http" env-required:"true"`
+	Env         string     `yaml:"env" env-default:"local"`
+	Http        HttpConfig `yaml:"http" env-required:"true"`
+	StoragePath string     `yaml:"storage_path" env-required:"true"`
 }
 
 type HttpConfig struct {
-	Port	int				`yaml:"port" env-default:"8000"`
-	Timeout time.Duration	`yaml:"timeout" env-default:"5s"`
+	Port    int           `yaml:"port" env-default:"8000"`
+	Timeout time.Duration `yaml:"timeout" env-default:"5s"`
 }
 
 func MustLoad() Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
